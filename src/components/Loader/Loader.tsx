83b5a962dcc892fe4125339702ffe38ac7569235
João Bezerra <joaobzzr@gmail.com>
2022-02-23 16:43:26 -0300

    Update

--- src/components/Loader/Loader.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import LoaderSvg from "$assets/images/loader.svg";
-import "./Loader.scss";
-
-export default function Loader() {
-    return <LoaderSvg className="Loader"/>;
-}
--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "$assets/css/reset.css";
-
-import App from "$components/app";
-
-ReactDOM.render(<App/>, document.getElementById("root"));
-

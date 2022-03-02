87e2c883076fcf680221ba9dd3ae543af062473e
João Bezerra <joaobzzr@gmail.com>
2022-03-02 19:59:19 -0300

    Add fahrenheit symbol to ImperialSystemToggle

--- src/components/ImperialSystemToggle/ImperialSystemToggle.tsx
+++ src/components/ImperialSystemToggle/ImperialSystemToggle.tsx
@@ -11,18 +11,21 @@ export default function ImperialSystemToggle(props: PropsType) {
     const { onToggleImperialSystem, usingImperialSystem } = props;
 
     return (
-        <div className="ImperialSystemToggle">
-            <input
-                type="checkbox"
-                className="ImperialSystemToggle_checkbox"
-                checked={usingImperialSystem}
-                onChange={() => {}}
-            />
-            <span
-                className="ImperialSystemToggle_slider"
-                onClick={() => onToggleImperialSystem(usingImperialSystem)}
-            >
-            </span>
+        <div className="ImperialSystemToggle_container">
+            <span className="ImperialSystemToggle_text">&#x00B0;F</span>
+            <div className="ImperialSystemToggle">
+                <input
+                    type="checkbox"
+                    className="ImperialSystemToggle_checkbox"
+                    checked={usingImperialSystem}
+                    onChange={() => {}}
+                />
+                <span
+                    className="ImperialSystemToggle_slider"
+                    onClick={() => onToggleImperialSystem(usingImperialSystem)}
+                >
+                </span>
+            </div>
         </div>
     );
 }
